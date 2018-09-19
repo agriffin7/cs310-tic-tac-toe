04a85a499aef59dfb185dd4b62904a6fc640b5ba
Austin <agriffin7@stu.jsu.edu>
2018-09-19 11:47:06 -0500

    Rewrote program, Stopped build from hanging

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,22 +1,19 @@
 package edu.jsu.mcis;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.util.Scanner;
 
-public class TicTacToeView extends JPanel{
+public class TicTacToeView extends JPanel implements ActionListener{
 	
 	private TicTacToeModel model;
-		
-	public TicTacToeView(){
-		super();
-	}
 	
-	@Override
-	protected void paintComponent(Graphics g) { //create GUI elements here
-		super.paintComponent(g);
-	}
-	
-	
+	private JButton[][] squares;
+	private JPanel squaresPanel = new JPanel(new GridLayout(model.getWidth(), model.getWidth()));;
+	private JLabel resultLabel = new JLabel("");
+	private String letter;
+	private String message;
+
     
     /* CONSTRUCTOR */
 	
@@ -31,30 +28,37 @@ public class TicTacToeView extends JPanel{
 		//the GUI
 		JFrame window = new JFrame("Tic-Tac-Toe");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().add(new TicTacToeView());
 		window.setSize(300,300);
-		window.setVisible(true);	
+		window.setVisible(true);
 
 		
 		//create the buttons
-		JPanel squaresPanel = new JPanel(new GridLayout(model.getWidth(), model.getWidth()));
 		
-		JButton squares[][] = new JButton[64][64];
 		for (int row = 0; row < model.getWidth(); row++){
 			for (int col = 0; col < model.getWidth(); col++){
-				squares[row][col] = new JButton();
+				squares[row][col] = new JButton("");
 				squares[row][col].setName("Square" + row + col);
-				squaresPanel.add(squares[row][col]);
 			}
 		}
+		for (int row = 0; row < model.getWidth(); row++){
+				for (int col = 0; col < model.getWidth(); col++){
+					squares[row][col].addActionListener(this);
+				}
+			}
+			for (int row = 0; row < model.getWidth(); row++){
+				for (int col = 0; col < model.getWidth(); col++){
+					squaresPanel.add(squares[row][col]);
+			}
+		}
+		
 		window.add(squaresPanel);
 		
-		JPanel resultLabelDisplay = new JPanel();
-		
-		JLabel resultLabel = new JLabel();
+		//create a JPanel		
+		JLabel resultLabel = new JLabel("");
 		resultLabel.setName("ResultLabel");
-		resultLabelDisplay.add(resultLabel);
-		window.add(resultLabelDisplay, BorderLayout.SOUTH);
+		message = "Welcome to Tic-Tac-Toe!";
+		resultLabel.setText(message);
+		window.add(resultLabel, BorderLayout.SOUTH);
     }
 		
 
@@ -79,5 +83,29 @@ public class TicTacToeView extends JPanel{
         System.out.println(r + "!");
 
     }
+	
+	public void actionPerformed(ActionEvent e){
+		//caculate who's turn it is
+		if (model.isXTurn()){
+			letter = "X";
+			squares[1][1].setText("Test");
+		}
+		else{
+			letter = "O";
+			squares[1][2].setText("Test2");
+		} 
+			
+		//set the letter
+		for (int i = 0; i < getWidth(); i++){
+			for (int j = 0; j < getWidth(); j++){
+				if (e.getSource() == squares[i][j]){
+					squares[i][j].setText(letter);
+				}
+				else{
+					squares[i][j].setText("ERROR!");
+				}
+			}
+		}
+	}
 	
 }
